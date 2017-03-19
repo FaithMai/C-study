a6dc14377703bd66492ef23eabde1d92bddc383e
FaithMai <faitmai@163.com>
2017-03-19 13:41:08 +0800

    lc

--- perfect number.cpp
+++ perfect number.cpp
@@ -18,7 +18,7 @@ int main()
 int judge_func(int m)   //用于判断是否为完数的函数
 {
 	int total = 0;
-	for (int i = 1; i <= (m+1) / 2; i++)
+	for (int i = 1; i <= m / 2; i++)
 	{
 		if (m%i == 0)
 			total += i;
@@ -32,12 +32,12 @@ void output_func(int m)  //用于将完数因子进行输出的函数
 {
 	int i = 1;
 	cout << m << "，" << "its factors are ";
-	while (i <= ((m+1) / 2))  // 此处用while循环为了让最后一个数字输出后，不输出逗号
+	while (i <= m / 2)  // 此处用while循环为了让最后一个数字输出后，不输出逗号
 	{
 		if (m%i == 0)
 			cout << i;
 		i++;
-		if (i <= ((m + 1) / 2) && m%i == 0)  // 对数字进行判断，如果是最后一个数字，就不输出逗号
+		if (i <= m / 2 && m%i == 0)  // 对数字进行判断，如果是最后一个数字，就不输出逗号
 			cout << "，";
 	}
 }
